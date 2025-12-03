99e4613cda9dc8300b9bda749287d7b1ce2c0b8b
LeonardoG <leonardoguevaraatehortua@gmail.com>
2025-12-03 17:43:20 -0500

    Fix Vercel deployment: add pages app, optimize config

--- ErgoProject/ErgoProject/settings.py
+++ ErgoProject/ErgoProject/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mqttApp'#.apps.MqttappConfig',
+    'mqttApp',  #.apps.MqttappConfig',
+    'pages',
 ]
 
 MIDDLEWARE = [
---
+++ ErgoProject/vercel_build.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+Script de build para Vercel
+Ejecuta migraciones automáticamente en cada deploy
+"""
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ErgoProject.settings')
+django.setup()
+
+from django.core.management import call_command
+
+# Ejecutar migraciones
+call_command('migrate', '--noinput')
+
+print("✅ Migraciones completadas")
